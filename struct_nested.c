c9ac3bf655fc81833fe169e33ab9f0e87e608d02
VisualStudy <sparksoldier@outlook.com>
2026-09-13 21:54:36 +0900

    nested

---
+++ struct_nested.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+
+typedef struct
+{
+    int year;
+    int month;
+    int day;
+} Date;
+
+typedef struct
+{
+    char name[20];
+    Date birthday; // 구조체 안에 구조체
+} Student;
+
+int main(void)
+{
+    Student s = {
+        "Kim",
+        {2005, 3, 15}
+    };
+
+    printf("이름: %s\n", s.name);
+    printf("생일: %d-%d-%d\n", s.birthday.year, s.birthday.month, s.birthday.day);
+
+    return 0;
+}
